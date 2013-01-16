2067c9d1057799b1961ac7a96885541f1cf4153c
Luze26 <yann.uzel@gmail.com>
2013-01-17 00:47:32 +0100

    Add bootstrap-sass and some css

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -12,17 +12,6 @@ class SectionsController < ApplicationController
     end
   end
 
-  # GET /sections/1
-  # GET /sections/1.json
-  def show
-    @section = Section.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @section }
-    end
-  end
-
   # GET /sections/new
   # GET /sections/new.json
   def new
